7c1baa8907db4f44956f65c285741a7dadc42de8
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-08-10 12:41:56 +0200

    ADD: provider and global style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { GlobalStyle } from './GlobalStyle';
+import store from "./store";
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
+  </ThemeProvider>
+</React.StrictMode>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
