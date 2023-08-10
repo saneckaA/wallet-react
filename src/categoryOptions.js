d762fc57ad277ac10b49064e6c4b8d0cb93f4e5c
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-08-10 12:38:49 +0200

    ADD:  category options for select input

---
+++ src/categoryOptions.js
@@ -0,0 +1,9 @@
+const categoryOptions = [
+    { label: "Food", value: "food", icon: "🍔" },
+    { label: "Shopping", value: "shopping", icon: "🛒" },
+    { label: "Transport", value: "transport", icon: "🚗"},
+    { label: "Cinema", value: "cinema", icon: "📽️"},
+    { label: "Income", value: "income", icon: "💵"},
+];
+
+export default categoryOptions;
