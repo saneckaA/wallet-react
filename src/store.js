ef01287eee702454f83382d42e301f0671552359
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-08-10 12:35:27 +0200

    ADD: store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import transactionsReducer from "./features/transactions/transactionsSlice";
+
+
+export default configureStore({
+    reducer: {
+        transactions: transactionsReducer,
+    }
+});
+
+
