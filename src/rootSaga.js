5ad74b8e48dcb00e3f6d450bf35730cf4a67f88f
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-11-11 14:03:08 +0100

    added theme saga to the root saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { transactionsSaga } from "./features/transactions/transactionsSaga";
+import { themeSaga } from "./themeSaga";
 
 export default function* rootSaga () {
     yield all([
         transactionsSaga(),
+        themeSaga(),
     ]);
 }
